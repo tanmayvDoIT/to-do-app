a7ae270080a75e92bc2fcc83e63f7068ad8dca24
tanmayvDoIT <tanmay.vdoit@gmail.com>
2021-05-14 00:47:44 +0530

    login card sm screen enlarged

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -36,66 +36,68 @@ const Login = (props) => {
 
   return (
     <div className="row min-vh-100 p-0 m-0">
-      <div className="col-6 m-auto p-0">
-        <div className="card border border-dark">
-          <div
-            className="card-header font-weight-bold text-center"
-            style={{ backgroundColor: '#cb9c18' }}
-          >
-            LOGIN
-          </div>
-          <div className="card-body">
-            <form onSubmit={submit}>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                {emailError !== null && (
-                  <div className="text-danger">{emailError}</div>
-                )}
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  placeholder="Enter email"
-                  autoComplete="on"
-                  value={email}
-                  onChange={(e) => {
-                    setemail(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                {passwordError !== null && (
-                  <div className="text-danger">{passwordError}</div>
-                )}
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                  autoComplete="on"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-              </div>
+      <div className="container m-auto">
+        <div className="col-md-6 col-sm-12 m-auto p-0">
+          <div className="card border border-dark">
+            <div
+              className="card-header font-weight-bold text-center"
+              style={{ backgroundColor: '#cb9c18' }}
+            >
+              LOGIN
+            </div>
+            <div className="card-body">
+              <form onSubmit={submit}>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  {emailError !== null && (
+                    <div className="text-danger">{emailError}</div>
+                  )}
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter email"
+                    autoComplete="on"
+                    value={email}
+                    onChange={(e) => {
+                      setemail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  {passwordError !== null && (
+                    <div className="text-danger">{passwordError}</div>
+                  )}
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                    autoComplete="on"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </div>
 
-              <button
-                type="submit"
-                className="btn btn-block font-weight-bold"
-                style={{ backgroundColor: '#cb9c18' }}
-              >
-                Submit
-              </button>
-            </form>
-            <div className="text-right mt-2">
-              <span
-                className="signup-btn"
-                onClick={() => props.history.push('/signup')}
-              >
-                Doesn't have an account?
-              </span>
+                <button
+                  type="submit"
+                  className="btn btn-block font-weight-bold"
+                  style={{ backgroundColor: '#cb9c18' }}
+                >
+                  Submit
+                </button>
+              </form>
+              <div className="text-right mt-2">
+                <span
+                  className="signup-btn"
+                  onClick={() => props.history.push('/signup')}
+                >
+                  Doesn't have an account?
+                </span>
+              </div>
             </div>
           </div>
         </div>
